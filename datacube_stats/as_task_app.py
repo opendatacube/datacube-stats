9b3bc2440b69b602c0c60e3930f03a2fcc2919cb
Damien Ayers <damien@omad.net>
2016-11-11 15:47:49 +1100

    Refactor task_runner core to take functions

--- datacube_stats/as_task_app.py
+++ datacube_stats/as_task_app.py
@@ -1,56 +1,50 @@
 from __future__ import absolute_import, print_function
 
 import click
-from datacube.ui.task_app import task_app, task_app_options, check_existing_files
+import itertools
+
+import logging
 
 _LOG = logging.getLogger('agdc-fc')
 
 
-@click.command(name=APP_NAME)
-@ui.pass_index(app_name=APP_NAME)
-@click.option('--dry-run', is_flag=True, default=False, help='Check if output files already exist')
-@click.option('--year', callback=validate_year, help='Limit the process to a particular year')
-@click.option('--queue-size', '--backlog', type=click.IntRange(1, 100000), default=3200,
-              help='Number of tasks to queue at the start')
-@task_app_options
-@task_app(make_config=make_fc_config, make_tasks=make_fc_tasks)
-def fc_app(index, config, tasks, executor, dry_run, queue_size, *args, **kwargs):
-    click.echo('Starting Stats processing...')
+def add_dataset_to_db(index, datasets):
+    for dataset in datasets.values:
+        index.datasets.add(dataset, skip_sources=True)
+        _LOG.info('Dataset added')
 
-    if dry_run:
-        check_existing_files((task['filename'] for task in tasks))
-    else:
-        results = []
-        task_queue = itertools.islice(tasks, queue_size)
-        for task in task_queue:
+
+def run_tasks(tasks, executor, queue_size, run_task, process_result):
+    click.echo('Starting processing...')
+    results = []
+    task_queue = itertools.islice(tasks, queue_size)
+    for task in task_queue:
+        _LOG.info('Running task: %s', task['tile_index'])
+        results.append(executor.submit(run_task, task=task))
+
+    click.echo('Task queue filled, waiting for first result...')
+
+    successful = failed = 0
+    while results:
+        result, results = executor.next_completed(results, None)
+
+        # submit a new task to replace the one we just finished
+        task = next(tasks, None)
+        if task:
             _LOG.info('Running task: %s', task['tile_index'])
-            results.append(executor.submit(do_fc_task, config=config, task=task))
+            results.append(executor.submit(run_task, task=task))
 
-        click.echo('Task queue filled, waiting for first result...')
+        # Process the result
+        try:
+            actual_result = executor.result(result)
+            process_result(actual_result)
+            successful += 1
+        except Exception as err:  # pylint: disable=broad-except
+            _LOG.exception('Task failed: %s', err)
+            failed += 1
+            continue
+        finally:
+            # Release the task to free memory so there is no leak in executor/scheduler/worker process
+            executor.release(result)
 
-        successful = failed = 0
-        while results:
-            result, results = executor.next_completed(results, None)
-
-            # submit a new task to replace the one we just finished
-            task = next(tasks, None)
-            if task:
-                _LOG.info('Running task: %s', task['tile_index'])
-                results.append(executor.submit(do_fc_task, config=config, task=task))
-
-            # Process the result
-            try:
-                datasets = executor.result(result)
-                for dataset in datasets.values:
-                    index.datasets.add(dataset, skip_sources=True)
-                    _LOG.info('Dataset added')
-                successful += 1
-            except Exception as err:  # pylint: disable=broad-except
-                _LOG.exception('Task failed: %s', err)
-                failed += 1
-                continue
-            finally:
-                # Release the task to free memory so there is no leak in executor/scheduler/worker process
-                executor.release(result)
-
-        click.echo('%d successful, %d failed' % (successful, failed))
+    click.echo('%d successful, %d failed' % (successful, failed))
