e582a78bc5d5711bdc2a5321203a8e143bf3b1a6
Damien Ayers <damien@omad.net>
2017-01-18 11:33:43 +1100

    Import numpy as np

--- datacube_stats/test_statistics.py
+++ datacube_stats/test_statistics.py
@@ -5,44 +5,44 @@ Tests for the custom statistics functions
 from __future__ import absolute_import
 
 from .statistics import nan_percentile, argpercentile, axisindex
-import numpy
+import numpy as np
 
 
 def test_nan_percentile():
     # create array of shape(5,100,100) - image of size 100x100 with 5 layers
-    test_arr = numpy.random.randint(0, 10000, 50000).reshape(5, 100, 100).astype(numpy.float32)
-    numpy.random.shuffle(test_arr)
+    test_arr = np.random.randint(0, 10000, 50000).reshape(5, 100, 100).astype(np.float32)
+    np.random.shuffle(test_arr)
     # place random NaNs
-    random_nans = numpy.random.randint(0, 50000, 500).astype(numpy.float32)
+    random_nans = np.random.randint(0, 50000, 500).astype(np.float32)
     for r in random_nans:
-        test_arr[test_arr == r] = numpy.NaN
+        test_arr[test_arr == r] = np.NaN
 
     # Test with single q
     q = 45
-    input_arr = numpy.array(test_arr, copy=True)
-    std_np_func = numpy.nanpercentile(input_arr, q=q, axis=0)
+    input_arr = np.array(test_arr, copy=True)
+    std_np_func = np.nanpercentile(input_arr, q=q, axis=0)
     new_func = nan_percentile(input_arr, q=q)
 
-    assert numpy.allclose(std_np_func, new_func)
+    assert np.allclose(std_np_func, new_func)
 
     # Test with all qs
     qs = range(0, 100)
-    input_arr = numpy.array(test_arr, copy=True)
-    std_np_func = numpy.nanpercentile(input_arr, q=qs, axis=0)
+    input_arr = np.array(test_arr, copy=True)
+    std_np_func = np.nanpercentile(input_arr, q=qs, axis=0)
     new_func = nan_percentile(input_arr, q=qs)
 
-    assert numpy.allclose(std_np_func, new_func)
+    assert np.allclose(std_np_func, new_func)
 
 
 def test_argpercentile():
     # Create random Data
-    test_arr = numpy.random.randint(0, 10000, 50000).reshape(5, 100, 100).astype(numpy.float32)
-    numpy.random.shuffle(test_arr)
+    test_arr = np.random.randint(0, 10000, 50000).reshape(5, 100, 100).astype(np.float32)
+    np.random.shuffle(test_arr)
     # place random NaNs
-    rand_nan = numpy.random.randint(0, 50000, 500).astype(numpy.float32)
+    rand_nan = np.random.randint(0, 50000, 500).astype(np.float32)
     for r in rand_nan:
-        test_arr[test_arr == r] = numpy.NaN
+        test_arr[test_arr == r] = np.NaN
 
-    np_result = numpy.nanpercentile(test_arr, q=25, axis=0, interpolation='nearest')
+    np_result = np.nanpercentile(test_arr, q=25, axis=0, interpolation='nearest')
     argpercentile_result = axisindex(test_arr, argpercentile(test_arr, q=25, axis=0), axis=0)
-    assert numpy.isclose(np_result, argpercentile_result).all()
+    assert np.isclose(np_result, argpercentile_result).all()
