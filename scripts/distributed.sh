208f2d629e031ead201531c1c3fb6febfd77dddd
Damien Ayers <damien@omad.net>
2017-03-07 13:59:59 +1100

    Update PBS distributed launcher script

--- scripts/distributed.sh
+++ scripts/distributed.sh
@@ -3,6 +3,9 @@
 # Use PBSDSH to start a dask-scheduler and a bunch of dask-workers, then
 # run the desired script to connect to them to run a job.
 #
+
+set -eu
+
 env_script=${module_dest}/scripts/environment.sh
 ppn=1
 tpp=1
@@ -54,21 +57,29 @@ SCHEDULER_NODE=`sed '1q;d' $PBS_NODEFILE`
 SCHEDULER_PORT=`shuf -i 2000-65000 -n 1`
 SCHEDULER_ADDR=$SCHEDULER_NODE:$SCHEDULER_PORT
 
+# Number of worker processes on Master Node
 n0ppn=$(( $ppn < $NCPUS-2 ? $ppn : $NCPUS-2 ))
 n0ppn=$(( $n0ppn > 0 ? $n0ppn : 1 ))
 
+# Run Dask Scheduler
+echo pbsdsh -n 0 -- /bin/bash -c "${init_env}; dask-scheduler --port $SCHEDULER_PORT ${bokeh_opt}"
 pbsdsh -n 0 -- /bin/bash -c "${init_env}; dask-scheduler --port $SCHEDULER_PORT ${bokeh_opt}"&
 sleep 5s
 
-pbsdsh -n 0 -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --nprocs ${n0ppn} --nthreads ${tpp}"&
+# Start Master Node Workers
+echo pbsdsh -n 0 -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --no-nanny --nprocs ${n0ppn} --nthreads ${tpp}"
+pbsdsh -n 0 -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --no-nanny --nprocs ${n0ppn} --nthreads ${tpp}"&
 sleep 0.5s
 
+# Start Workers on Other Nodes
 for ((i=NCPUS; i<PBS_NCPUS; i+=NCPUS)); do
-  pbsdsh -n $i -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --nprocs ${ppn} --nthreads ${tpp}"&
+  echo pbsdsh -n $i -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --no-nanny --nprocs ${ppn} --nthreads ${tpp}"
+  pbsdsh -n $i -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --no-nanny --nprocs ${ppn} --nthreads ${tpp}"&
   sleep 0.5s
 done
 sleep 5s
 
+# Run main application on Master Node
 echo "*** APPLICATION ***"
 echo "${@/DSCHEDULER/${SCHEDULER_ADDR}}"
 
