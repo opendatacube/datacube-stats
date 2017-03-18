d8406299840a0346979f2336c9742c3b9998dad3
Damien Ayers <damien@omad.net>
2017-03-18 14:35:05 +1100

    Improve output driver logging

--- tests/test_output_drivers.py
+++ tests/test_output_drivers.py
@@ -7,9 +7,9 @@ from datacube.utils.geometry import GeoBox, CRS
 from datacube_stats.models import OutputProduct
 from datacube_stats.output_drivers import GeotiffOutputDriver
 from datacube_stats.statistics import NoneStat
-from datacube_stats.main import StatsTask
 from datetime import datetime
 import pytest
+import rasterio
 
 
 @pytest.mark.parametrize("dtype,nodata", [
@@ -19,38 +19,53 @@ import pytest
     ('float32', np.nan),
     ('float64', np.nan),
 ])
-def test_rio(tmpdir, dtype, nodata):
+def test_geotiff_outputs(tmpdir, dtype, nodata):
     size = 64
-    chunk_size = int(size/2)
-    m = MetadataType({}, {})
+    chunk_size = int(size / 2)
+    metadata_type = MetadataType({}, {})
     storage = {'chunking': {'x': chunk_size, 'y': chunk_size}}
-    # input_measurement = MagicMock()
-    input_measurement = {'name': 'sample_input_measurement',
-                         'dtype': dtype,
-                         'nodata': nodata,
-                         'units': '1'}
-    output_product = OutputProduct(m,
-                                   input_measurements=[input_measurement],
+
+    input_measurement_1 = {'name': 'sample_input_measurement_1',
+                           'dtype': dtype,
+                           'nodata': nodata,
+                           'units': '1'}
+    input_measurement_2 = {'name': 'sample_input_measurement_2',
+                           'dtype': dtype,
+                           'nodata': nodata,
+                           'units': '1'}
+    output_product = OutputProduct(metadata_type,
+                                   input_measurements=[input_measurement_1, input_measurement_2],
                                    storage=storage,
                                    name='test_product',
-                                   file_path_template='foo_bar.tiff',
+                                   file_path_template='foo_bar.tif',
                                    stat_name='',
                                    statistic=NoneStat())
-    # dtypes = set(m['dtype'] for m in self._output_products[out_prod_name].product.measurements.values())
-
-    # task = StatsTask(time_period=(1, 2), tile_index=None, sources=None, output_products=[output_product])
     task = MagicMock()
-    task.geobox = GeoBox(size, size, Affine(1/size, 0.0, 151.0, 0.0, -1/size, -29.0), CRS('EPSG:4326'))
+    task.geobox = GeoBox(size, size, Affine(1 / size, 0.0, 151.0, 0.0, -1 / size, -29.0), CRS('EPSG:4326'))
     task.output_products = {'sample_prod': output_product}
     task.tile_index = 0, 0
     task.time_period = datetime.now(), datetime.now()
-    output_path = str(tmpdir)
 
     tile_index = (None, slice(0, chunk_size, None), slice(0, chunk_size, None))
     values = np.full((chunk_size, chunk_size), fill_value=7, dtype=dtype)
 
-    with GeotiffOutputDriver(task=task, storage=storage, output_path=output_path) as output_driver:
-        # Why isn't get_dtype being called when opening the file? ... It is
-        output_driver.write_data(prod_name='sample_prod', measurement_name='sample_input_measurement',
+    #
+    # Run the code we want to test
+    #
+    with GeotiffOutputDriver(task=task, storage=storage, output_path=str(tmpdir)) as output_driver:
+        output_driver.write_data(prod_name='sample_prod', measurement_name='sample_input_measurement_1',
                                  tile_index=tile_index, values=values)
+
+    # Check that it had the desired result
+    output_filename = tmpdir / 'foo_bar.tif'
+    assert output_filename.exists()
+    with rasterio.open(str(output_filename)) as src:
+        assert src.width == size
+        assert src.height == size
+        for i in range(1, 3):
+            tags = src.tags(i)
+            assert tags['name'] == 'sample_input_measurement_%s' % i
+            assert 'end_date' in tags
+            assert 'start_date' in tags
+            assert 'source_product' in tags
 
