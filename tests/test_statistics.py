5f8940deaddc26d205bf7f95e87866b8d2e982ae
Damien Ayers <damien@omad.net>
2017-11-08 12:39:48 +1100

    Add tests for WofsStat and NormalisedDifferenceStat
    Uses hypothesis to generate examples, to be rolled out to other test cases.

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -4,12 +4,18 @@ Tests for the custom statistics functions
 """
 from __future__ import absolute_import
 
-from datacube_stats.statistics import nan_percentile, argpercentile, axisindex
-import datacube_stats.statistics
-import numpy as np
-import xarray as xr
+import string
 
+import hypothesis.strategies as st
+import numpy as np
 import pytest
+import xarray as xr
+from hypothesis import given
+
+import datacube_stats.statistics
+from datacube.utils.geometry import CRS
+from datacube_stats.statistics import nan_percentile, argpercentile, axisindex, NormalisedDifferenceStats, WofsStats, \
+    StatsConfigurationError
 
 
 def test_nan_percentile():
@@ -98,3 +104,100 @@ def test_new_geometric_median():
 
     # The two bands had the same inputs, so should have the same result
     assert (result.band1 == result.band2).all()
+
+
+DatacubeCRSStrategy = st.sampled_from([CRS('EPSG:4326'), CRS('EPSG:3577'), CRS('EPSG:28354')])
+
+
+@st.composite
+def dataset_shape(draw):
+    crs = draw(DatacubeCRSStrategy)
+    height = draw(st.integers(10, 400))
+    width = draw(st.integers(10, 400))
+    times = draw(st.integers(1, 10))
+    return crs, height, width, times
+
+
+invalid_names = ('latitude', 'longitude', 'crs', 'x', 'y')
+variable_name = st.text(min_size=1, alphabet=string.ascii_letters + '_').filter(lambda n: n not in invalid_names)
+
+
+@st.composite
+def eo_wofs_dataset(draw):
+    crs, height, width, times = draw(dataset_shape())
+    arr = np.random.randint(low=0, high=255, size=(times, height, width), dtype='uint8')
+
+    data_array_1 = xr.DataArray(arr,
+                                dims=('time',) + crs.dimensions,
+                                coords={'time': list(range(times))},
+                                attrs={'crs': crs})
+    dataset = xr.Dataset(data_vars={'water': data_array_1},
+                         attrs={'crs': crs})
+
+    return dataset
+
+
+@given(eo_wofs_dataset())
+def test_wofs_stats(dataset):
+    wofsstat = WofsStats()
+    # Check that measurements() does something useful
+    output_measurements = wofsstat.measurements([{'name': 'water'}])
+    assert len(output_measurements) == 3
+    expected_vars = {'count_wet', 'count_clear', 'frequency'}
+    output_names = set(m['name'] for m in output_measurements)
+    assert expected_vars == output_names
+
+    # Check the computation returns something reasonable
+    result = wofsstat.compute(dataset)
+
+    assert result
+    assert 'time' not in result.dims
+    assert all(result_dim in dataset.dims for result_dim in result.dims)
+    assert dataset.crs == result.crs
+
+    assert set(result.data_vars) == expected_vars
+
+
+@st.composite
+def two_band_eo_dataset(draw):
+    crs, height, width, times = draw(dataset_shape())
+    arr = np.random.random_sample(size=(times, height, width))
+
+    data1 = xr.DataArray(arr,
+                         dims=('time',) + crs.dimensions,
+                         coords={'time': list(range(times))},
+                         attrs={'crs': crs})
+    arr = np.random.random_sample(size=(times, height, width))
+    data2 = xr.DataArray(arr,
+                         dims=('time',) + crs.dimensions,
+                         coords={'time': list(range(times))},
+                         attrs={'crs': crs})
+    name1, name2 = draw(st.tuples(variable_name, variable_name))
+    dataset = xr.Dataset(data_vars={variable_name: data1, name2: data2},
+                         attrs={'crs': crs})
+    return dataset
+
+
+@given(two_band_eo_dataset(), variable_name)
+def test_normalised_difference_stats(dataset, output_name):
+    var1, var2 = list(dataset.data_vars)
+    ndstat = NormalisedDifferenceStats(var1, var2, output_name)
+    result = ndstat.compute(dataset)
+
+    assert result
+    assert 'time' not in result.dims
+    assert dataset.crs == result.crs
+
+    expected_output_varnames = set(f'{output_name}_{stat_name}' for stat_name in ndstat.stats)
+    assert set(result.data_vars) == expected_output_varnames
+
+    # Check the measurements() function raises an error on bad input_measurements
+    with pytest.raises(StatsConfigurationError):
+        invalid_names = [{'name': 'foo'}]
+        ndstat.measurements(invalid_names)
+
+    # Check the measurements() function returns something reasonable
+    input_measurements = [{'name': name} for name in (var1, var2)]
+    output_measurements = ndstat.measurements(input_measurements)
+    measurement_names = set(m['name'] for m in output_measurements)
+    assert expected_output_varnames == measurement_names
