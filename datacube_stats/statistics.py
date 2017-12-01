10bd4b1ae6d71cb9e72be30bc9471bb33d49eec5
Belle <belletissott@yahoo.com>
2017-12-01 12:56:42 +1100

    Implement new smad function in stats. (#53)

--- datacube_stats/statistics.py
+++ datacube_stats/statistics.py
@@ -843,3 +843,70 @@ try:
 
 except ImportError:
     pass
+
+
+try:
+    import pcm
+
+    class SpectralMAD(Statistic):
+        def __init__(self, eps=1e-3):
+            super(SpectralMAD, self).__init__()
+            self.eps = eps
+
+        def compute(self, data):
+            """
+            :param xarray.Dataset data:
+            :return: xarray.Dataset
+            """
+   
+            # We need to reshape our data into Y, X, Band, Time
+            squashed_together_dimensions, output_dimensions = self._vars_to_transpose(data)
+
+            squashed = data.to_array(dim='variable').transpose(*squashed_together_dimensions)
+            assert squashed.dims == squashed_together_dimensions
+
+            # Grab a copy of the coordinates we need for creating the output DataArray
+            output_coords = copy(squashed.coords)
+            del output_coords['time']
+            del output_coords['variable']
+
+            # Call Dale's geometric median & spectral mad functions here
+            gm = pcm.gmpcm(squashed.data)
+            squashed = pcm.smad(squashed.data, gm)
+            
+            # Jam the raw numpy array back into a pleasantly labelled DataArray
+            as_datarray = xarray.DataArray(squashed, dims=output_dimensions, coords=output_coords)
+
+            return as_datarray.transpose(*output_dimensions).to_dataset(dim='smad')
+
+        @staticmethod
+        def _vars_to_transpose(data):
+            """
+            We need to be able to handle data given to use in either Geographic or Projected form.
+
+            The Data Cube provided xarrays will contain different dimensions, latitude/longitude or x/y, which means
+            the array reshaping takes different arguments.
+
+            The dimension ordering returned by this function is specific to the spectral median absolute deviation 
+            function included from the `pcm` module.
+
+            :return: pcm input array dimension order, datacube dimension ordering
+            """
+            is_projected = 'x' in data.dims and 'y' in data.dims
+            is_geographic = 'longitude' in data.dims and 'latitude' in data.dims
+
+            if is_projected and is_geographic:
+                raise StatsProcessingError('Data to process contains BOTH geographic and projected dimensions, '
+                                           'unable to proceed')
+            elif not is_projected and not is_geographic:
+                raise StatsProcessingError('Data to process contains NEITHER geographic nor projected dimensions, '
+                                           'unable to proceed')
+            elif is_projected:
+                return ('y', 'x', 'variable', 'time'), ('y', 'x')
+            else:
+                return ('latitude', 'longitude', 'variable', 'time'), ('variable', 'latitude', 'longitude')
+
+    STATS['spectral_mad'] = SpectralMAD
+except ImportError:
+    pass
+
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -96,7 +96,6 @@ def test_new_geometric_median():
     # The two bands had the same inputs, so should have the same result
     assert (result.band1 == result.band2).all()
 
-
 def test_new_med_ndwi():
     medndwi = NormalisedDifferenceStats('green', 'nir', 'ndwi', stats=['median'])
 
@@ -284,3 +283,19 @@ def test_incremental_computations(dataset, xarray_func, incremental_fn):
     std_result = getattr(dataset, xarray_func)(dim='time')
 
     assert inc_result == std_result
+
+
+@pytest.mark.skipif(not hasattr(datacube_stats.statistics, 'SpectralMAD'),
+			reason='requires `pcm` module for spectral mad')
+def test_smad():
+    from datacube_stats.statistics import SpectralMAD
+    
+    arr = np.random.random((5, 100, 100))
+
+    dataarray = xr.DataArray(arr, dims=('time', 'y', 'x'), coords={'time': list(range(5))})
+    dataset = xr.Dataset(data_vars={'band1': dataarray, 'band2': dataarray})
+
+    smad = SpectralMAD()
+    result = smad.compute(dataset)
+
+    assert result
