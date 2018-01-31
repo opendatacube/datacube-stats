15678d7ae7a40e5f54b0a6c0a02b3da80dd779c7
Imam Tashdid ul Alam <imam.alam@gmail.com>
2018-01-31 23:24:00 +1100

    code comment about possible bug

--- datacube_stats/tasks.py
+++ datacube_stats/tasks.py
@@ -254,6 +254,7 @@ class NonGriddedTaskGenerator(object):
             all_source_times = sorted(all_source_times)
             extra_fn_args, filtered_times = \
                 get_filter_product(self.filter_product, self.feature[0], all_source_times, date_ranges)
+                # TODO: the `self.feature[0]` bit looks plain wrong!
             _LOG.info("Filtered times %s", filtered_times)
             task = self.set_task(task, input_region, filtered_times, extra_fn_args)
         else:
