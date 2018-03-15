91aa3bf7a6da4f3dd7d7007914c43053622db6a4
Imam Tashdid ul Alam <imam.alam@gmail.com>
2018-03-15 14:28:58 +1100

    release 0.9a9

--- datacube_stats/version.py
+++ datacube_stats/version.py
@@ -5,4 +5,4 @@ defined in datacube_stats/__init__.py then install would fail because there are 
 dependencies imported in datacube_stats/__init__.py that are not present until after
 install. Do not import anything into this module."""
 
-__version__ = '0.9a8'
+__version__ = '0.9a9'
