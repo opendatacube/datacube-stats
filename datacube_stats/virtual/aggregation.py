3fba8aa5260169bdce1b09f1651bd200bd7345f3
Emma Ai <emma.ai@ga.gov.au>
2019-05-24 18:44:56 +1000

    add geomedian to virtual

--- datacube_stats/virtual/aggregation.py
+++ datacube_stats/virtual/aggregation.py
@@ -6,6 +6,7 @@ import dask.array as da
 from collections import Sequence
 from datacube.virtual.impl import Transformation, Measurement
 from datacube_stats.stat_funcs import argpercentile,  axisindex
+from pcm import gmpcm
 
 
 class Percentile(Transformation):
@@ -48,11 +49,9 @@ class Percentile(Transformation):
         data = data.chunk({'time': -1})
         if self.quality_band is not None:
             quality_count =  (data[self.quality_band].data == True).sum(axis=0)
-        for var in data.data_vars:
-            if self.quality_band is not None:
-                if var == self.quality_band:
-                    continue
+            data = data.drop(self.quality_band)
 
+        for var in data.data_vars:
             if data[var].dtype == 'int8':
                 data_type = 'int16'
             else:
@@ -111,3 +110,75 @@ class Percentile(Transformation):
             for q in self.qs:
                 renamed[key + '_PC_' + str(q)] = Measurement(**{**m, 'name': key + '_PC_' + str(q), 'dtype': data_type})
         return renamed
+    
+
+class NewGeomedianStatistic(Transformation):
+    """
+        geomedian
+    """
+    def __init__(self, eps=1e-3, num_threads=None):
+        self.eps = eps
+        self.num_threads = num_threads
+
+    def compute(self, data):
+        """
+        :param xarray.Dataset data:
+        :return: xarray.Dataset
+        """
+        dtypes = {}
+        for var in data.data_vars:
+            dtypes[var] = data[var].dtype
+            nodata = getattr(data[var], 'nodata', None)
+            if nodata is not None:
+                data[var] = data[var].where(data[var] > nodata)
+            else:
+                data[var] = data[var].astype(np.float)
+        # We need to reshape our data into Time, Y, X, Band
+        squashed_together_dimensions, normal_datacube_dimensions = self._vars_to_transpose(data)
+
+        squashed = data.to_array(dim='variable').transpose(*squashed_together_dimensions)
+        assert squashed.dims == squashed_together_dimensions
+
+        # Call Dale's function here
+        squashed = squashed.chunk({'time':-1, 'variable': -1})
+        result = xr.apply_ufunc(lambda x, num_threads: gmpcm(x, num_threads=num_threads), squashed,
+                                kwargs={'num_threads': self.num_threads},
+                                input_core_dims=[['time']], output_dtypes=[np.float],
+                                dask='parallelized', keep_attrs=True)
+        squashed = result.to_dataset(dim='variable')
+        for var in  squashed.data_vars:
+            squashed[var] = squashed[var].astype(dtypes[var])
+            squashed[var].attrs = data[var].attrs.copy()
+
+        return squashed 
+
+    @staticmethod
+    def _vars_to_transpose(data):
+        """
+        We need to be able to handle data given to use in either Geographic or Projected form.
+
+        The Data Cube provided xarrays will contain different dimensions, latitude/longitude or x/y, which means
+        the array reshaping takes different arguments.
+
+        The dimension ordering returned by this function is specific to the Geometric Median PCM functions
+        included from the `pcm` module.
+
+        :return: pcm input array dimension order, datacube dimension ordering
+        """
+        is_projected = 'x' in data.dims and 'y' in data.dims
+        is_geographic = 'longitude' in data.dims and 'latitude' in data.dims
+
+        if is_projected and is_geographic:
+            raise StatsProcessingError('Data to process contains BOTH geographic and projected dimensions, '
+                                       'unable to proceed')
+        elif not is_projected and not is_geographic:
+            raise StatsProcessingError('Data to process contains NEITHER geographic nor projected dimensions, '
+                                       'unable to proceed')
+        elif is_projected:
+            return ('time', 'y', 'x', 'variable'), ('variable', 'y', 'x')
+        else:
+            return ( 'time', 'latitude', 'longitude', 'variable'), ('variable', 'latitude', 'longitude')
+
+    def measurements(self, input_measurements):
+        return input_measurements
+
---
+++ datacube_stats/virtual/test_transforms.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pytest
+import xarray as xr
+import dask.array as da
+from datetime import datetime, timedelta
+from datacube_stats.virtual.aggregation import Percentile, NewGeomedianStatistic
+from datacube_stats.virtual.external import MangroveCC, TCIndex, MaskByValue
+
+x_range = (-33000, -23000)
+y_range = (-1310000, -1300000)
+
+time_range = (datetime(2016,1,1), datetime(2016, 12, 31))
+time_step = timedelta(days=8)
+crs = 'EPSG:3577'
+resolution = (25, -25)
+
+def test_percentile():
+    x_coords = np.arange(x_range[0], x_range[1], resolution[0]).astype(np.float)
+    y_coords = np.arange(y_range[0], y_range[1], resolution[0]).astype(np.float)
+    time = np.arange(time_range[0], time_range[1], time_step)
+    bands = []
+    for i in range(2):
+        data = da.random.random(time.shape+x_coords.shape+y_coords.shape, chunks=(1, 200, 200))
+        data = (100 * data - 5).astype(np.int16)
+        data[data < 0] = -1
+        bands.append(xr.DataArray(data, name='band' + str(i+1), dims=('time', 'y', 'x'),
+            coords={'time': time, 'y': y_coords, 'x': x_coords}, attrs={'crs': crs, 'nodata': -1, 'dtype': np.int16}))
+
+    data = da.random.random(time.shape+x_coords.shape+y_coords.shape, chunks=(1, 200, 200))
+    data = (data - .5 > 0).astype(np.bool)
+    bands.append(xr.DataArray(data, name='bandq', dims=('time', 'y', 'x'),
+            coords={'time': time, 'y': y_coords, 'x': x_coords}, attrs={'crs': crs, 'nodata': -1, 'dtype': np.bool}))
+
+    input_data = xr.merge(bands)
+    input_data.attrs['crs'] = crs
+
+    percentile = Percentile(q=[10, 50, 90], minimum_valid_observations=3, not_sure_mark=2, quality_band='bandq')
+    output_data = percentile.compute(input_data)
+    return output_data
+
+
+def test_mangroves():
+    x_coords = np.arange(x_range[0], x_range[1], resolution[0]).astype(np.float)
+    y_coords = np.arange(y_range[0], y_range[1], resolution[0]).astype(np.float)
+    time = np.array(time_range[0], dtype=np.datetime64).reshape(1,)
+    data = da.random.random(time.shape+x_coords.shape+y_coords.shape, chunks=(1, 200, 200))
+    data = (100 * data - 5).astype(np.int16)
+    data[data < 0] = -1
+    band =  xr.DataArray(data, name='band1', dims=('time', 'y', 'x'),
+        coords={'time': time, 'y': y_coords, 'x': x_coords}, attrs={'crs': crs, 'nodata': -1, 'dtype': np.int16})
+    band = band.to_dataset()
+    band.attrs['crs'] = crs
+    mangroves = MangroveCC(thresholds=[15, 40, 62], shape_file='maximum_extent_of_mangroves_Apr2019.shp')
+    output_data = mangroves.compute(band)
+    return output_data
+
+def test_tci():
+    x_coords = np.arange(x_range[0], x_range[1], resolution[0]).astype(np.float)
+    y_coords = np.arange(y_range[0], y_range[1], resolution[0]).astype(np.float)
+    time = np.array(time_range[0], dtype=np.datetime64).reshape(1,)
+    band_names = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
+    bands = []
+    for band in band_names:
+        data = da.random.random(time.shape+x_coords.shape+y_coords.shape, chunks=(1, 200, 200))
+        data = (10000 * data - 5).astype(np.int16)
+        data[data <= 0] = -9999
+        bands.append(xr.DataArray(data, name=band, dims=('time', 'y', 'x'),
+            coords={'time': time, 'y': y_coords, 'x': x_coords}, attrs={'crs': crs, 'nodata': -9999, 'dtype': np.int16}))
+    input_data = xr.merge(bands)
+    input_data.attrs['crs'] = crs
+    tci = TCIndex()
+    output_data = tci.compute(input_data)
+    return output_data
+
+def test_maskbyvalue():
+    x_coords = np.arange(x_range[0], x_range[1], resolution[0]).astype(np.float)
+    y_coords = np.arange(y_range[0], y_range[1], resolution[0]).astype(np.float)
+    time = np.array(time_range[0], dtype=np.datetime64).reshape(1,)
+    data = da.random.random(time.shape+x_coords.shape+y_coords.shape, chunks=(1, 200, 200))
+    data = (100 * data - 5).astype(np.int16)
+    data[data < 0] = -1
+    band =  xr.DataArray(data, name='band1', dims=('time', 'y', 'x'),
+        coords={'time': time, 'y': y_coords, 'x': x_coords}, attrs={'crs': crs, 'nodata': -1, 'dtype': np.int16})
+    band = band.to_dataset()
+    band.attrs['crs'] = crs
+    try:
+        mask = MaskByValue(mask_measurement_name='band1', greater_than=50, smaller_than=49)
+    except:
+        print('test exception')
+    mask = MaskByValue(mask_measurement_name='band1', greater_than=50)
+    output_data = mask.compute(band)
+    return output_data
+
+def test_geomedian():
+    x_coords = np.arange(x_range[0], x_range[1], resolution[0]).astype(np.float)
+    y_coords = np.arange(y_range[0], y_range[1], resolution[0]).astype(np.float)
+    time = np.arange(time_range[0], time_range[1], time_step)
+    band_names = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
+    bands = []
+    for band in band_names:
+        data = da.random.random(time.shape+x_coords.shape+y_coords.shape, chunks=(1, 200, 200))
+        data = (10000 * data - 5).astype(np.int16)
+        data[data <= 0] = -9999
+        bands.append(xr.DataArray(data, name=band, dims=('time', 'y', 'x'),
+            coords={'time': time, 'y': y_coords, 'x': x_coords}, attrs={'crs': crs, 'nodata': -9999, 'dtype': np.int16}))
+    input_data = xr.merge(bands)
+    input_data.attrs['crs'] = crs
+    geomedian = NewGeomedianStatistic(num_threads=2)
+    output_data = geomedian.compute(input_data)
+    return output_data
+
